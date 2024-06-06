fd89c25b01cc6b6b84e53ad9bbcf21ec6dbea763
Denis <info@dlex.live>
2024-06-06 20:03:07 +0200

    Terminato UI/UX del login

--- src/main/java/dlex/dashboardjavafx/HelloApplication.java
+++ src/main/java/dlex/dashboardjavafx/HelloApplication.java
@@ -11,7 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 400, 550);
+        stage.setResizable(false);
         stage.setTitle("Dashboard Aziendale");
         stage.setScene(scene);
         stage.show();
--- src/main/java/dlex/dashboardjavafx/HelloController.java
+++ src/main/java/dlex/dashboardjavafx/HelloController.java
@@ -2,13 +2,21 @@ package dlex.dashboardjavafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private TextField username;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField password;
+    @FXML
+    private Label message;
+    @FXML
+    protected void login() {
+        if (username.getText().equals("admin") && password.getText().equals("admin")) {
+            message.setText("Login successful");
+        } else {
+            message.setText("Login failed");
+        }
     }
 }
