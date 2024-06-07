8ab16bea446cfcb5fcc32b0deb03c796d190b96e
Denis <info@dlex.live>
2024-06-07 16:56:25 +0200

    Prime UI della dashboard.fxml

--- src/main/java/dlex/dashboardjavafx/Script.java
+++ src/main/java/dlex/dashboardjavafx/Script.java
@@ -3,6 +3,10 @@ package dlex.dashboardjavafx;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -17,6 +21,12 @@ public class Script {
     @FXML
     private Label message;
     @FXML
+    private CategoryAxis grafico;
+    @FXML
+    private NumberAxis graficoNum;
+    @FXML
+    private LineChart<String, Number> lineChart;
+    @FXML
     protected void login() {
         if (username.getText().equals("denis") && password.getText().equals("denis1234")) {
             try {
@@ -28,10 +38,6 @@ public class Script {
             message.setText("Credenziali errate");
         }
     }
-    @FXML
-    protected void test() {
-        System.out.println("Test");
-    }
     public void dashboard() throws IOException {
         ((Stage)username.getScene().getWindow()).close();
         Stage stage = new Stage();
@@ -41,5 +47,36 @@ public class Script {
         stage.setTitle("DASHBOARD | Dashboard Aziendale");
         stage.setScene(scene);
         stage.show();
+    }
+    @FXML
+    protected void graficoFunz() {
+        grafico.setLabel("Mesi");
+        graficoNum.setLabel("Vendite");
+        lineChart.setTitle("Vendite Mensili");
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Vendite");
+        series.getData().add(new XYChart.Data<>("Gennaio", 1000));
+        series.getData().add(new XYChart.Data<>("Febbraio", 2000));
+        series.getData().add(new XYChart.Data<>("Marzo", 3000));
+        series.getData().add(new XYChart.Data<>("Aprile", 4000));
+        series.getData().add(new XYChart.Data<>("Maggio", 1000));
+        series.getData().add(new XYChart.Data<>("Giugno", 6000));
+        series.getData().add(new XYChart.Data<>("Luglio", 5000));
+        series.getData().add(new XYChart.Data<>("Agosto", 8000));
+        series.getData().add(new XYChart.Data<>("Settembre", 9000));
+        lineChart.getData().add(series);
+    }
+    @FXML
+    protected void dipendenti(){
+        System.out.println("Dipendenti");
+    }
+    @FXML
+    protected void home(){
+        System.out.println("Home");
+        graficoFunz();
+    }
+    @FXML
+    protected void regAtt(){
+        System.out.println("RegAtt");
     }
 }
